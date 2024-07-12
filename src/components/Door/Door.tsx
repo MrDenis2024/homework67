8750d682844029ac6432be66999b74e74cca1d48
Khrunev Denis <deniskhrunev@gmail.com>
2024-07-12 13:47:28 +0600

    Создан структура по добавлению и удалению символов а так же проверка на правильный пароль от двери

--- src/components/Door/Door.tsx
+++ src/components/Door/Door.tsx
@@ -1,17 +1,35 @@
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {RootState} from '../../app/store';
+import {add, del, entry, reset} from './doorSlice';
 import './Door.css';
 
 const Door = () => {
   const doorValue = useSelector((state: RootState) => state.door);
+  const dispatch = useDispatch();
+
+  const btnClick = (value: string) => {
+    if (value === '<') {
+      dispatch(del());
+    } else if (value === 'E') {
+      dispatch(entry());
+    } else {
+      dispatch(add(value));
+    }
+  };
+
   return (
     <div className='door'>
-      <div className='door-pass'>
-
+      <div className='door-pass' style={{backgroundColor: doorValue.bgColor}}>
+        {doorValue.message ? (
+          <>{doorValue.message}</>
+        ) : (
+          <>{doorValue.password.replace(/./g, '*')}</>
+        )}
       </div>
       {doorValue.symbol.map((symbol) => (
-        <button key={symbol}>{symbol}</button>
+        <button key={symbol} onClick={() => btnClick(symbol)}>{symbol}</button>
       ))}
+      <button onClick={() => dispatch(reset())}>Reset</button>
     </div>
   );
 };
--- src/components/Door/doorSlice.ts
+++ src/components/Door/doorSlice.ts
@@ -1,19 +1,51 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
 export interface DoorState {
   symbol: string[],
+  password: string;
+  bgColor: string;
+  message: string;
 }
 
 const initialState: DoorState = {
   symbol: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '<', '0', 'E'],
+  password: '',
+  bgColor: '',
+  message: '',
 };
+
+const correctPass = '1337';
 
 export  const doorSlice = createSlice({
   name: "door",
   initialState,
   reducers: {
-
+    add: (state, action: PayloadAction<string>) => {
+      if(state.password.length < 4 && state.message === '') {
+        state.password += action.payload;
+      }
+    },
+    del: (state) => {
+      if(state.message === '') {
+        state.password = state.password.slice(0, -1);
+      }
+    },
+    entry: (state) => {
+      if(state.password === correctPass) {
+        state.bgColor = 'green';
+        state.message = 'Access Granted';
+      } else {
+        state.bgColor = 'red';
+        state.message = 'Access Denied';
+      }
+    },
+    reset: (state) => {
+      state.password = '';
+      state.bgColor = '';
+      state.message = '';
+    },
   },
 });
 
 export const doorReducer = doorSlice.reducer;
+export const {add, del, entry, reset} = doorSlice.actions;
